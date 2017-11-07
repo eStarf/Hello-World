8630c4f415fe0c6e801b2097ac39b1ca7f185dd8
eStarf <32592614+eStarf@users.noreply.github.com>
2017-11-07 09:02:38 +0800

    Add files via upload

---
+++ 比较3数大小.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	printf("输入三个数："); 
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b){
+		if(b>c)
+		printf("%d,%d,%d\n",a,b,c);
+		else if(a>c)
+		printf("%d%d%d\n",a,c,b);
+	    else printf("%d%d%d\n",c,a,b);
+	}
+    else {
+	     if(c>b){
+	     printf("%d%d%d\n",c,b,a);
+	     }
+	     else if(a>c){
+	     printf("%d%d%d\n",b,a,c);
+       	 }
+	     else printf("%d%d%d\n",b,c,a);
+	 }
+    return 0;
+}
